174e12a6363d90b7995bf58091a9ae4a4eefe1b9
Sophie Crowley <sophiecrowley98@gmail.com>
2019-03-25 21:20:06 +0000

    verb game should now work

--- word_gap_game.cpp
+++ word_gap_game.cpp
@@ -11,7 +11,7 @@ vector<string> dictionaryThree;
 
 bool check_word(string word);
 void run_game();
-void run_verb_game(vector<string> test_words);
+int run_verb_game(vector<string> test_words);
 int highScore = 0;
 void run_game();
 void game_choice();
@@ -84,9 +84,39 @@ void game_choice() {
 
 }
 
-void run_verb_game(vector<string> test_words)
+int run_verb_game(vector<string> test_words)
 {
-
+  int score = 0;
+  string person;
+  string conjugation;
+  string verb;
+  size_t verbPos;
+  size_t personPos;
+  string current;
+  for(int i = 0; i < 10; i++) {
+    current = test_words[i];
+    verbPos = current.find('/');
+    verb = current.substr(0, verbPos);
+    personPos = current.find('/', verbPos+1);
+    person = current.substr(verbPos+1,personPos-verbPos-1);
+    conjugation = current.substr(personPos+1);
+    int word_pos;
+    word_pos = rand() % conjugation.size(); // so 0 1 2
+    conjugation[word_pos] = '_';
+    char c;
+    cout << "The verb is " << verb << endl;
+    cout << "Fill in the gap:" << person << " " << conjugation << endl;
+    cin >> c;
+    conjugation[word_pos] = c;
+    if(conjugation==current.substr(personPos+1)) {
+      cout << "Valid answer\n";
+      score++;
+    }
+    else {
+      cout << "Invalid answer\n";
+    }
+  }
+  return score;
 }
 void run_game() {
    vector<string> test_words;
@@ -99,7 +129,7 @@ void run_game() {
   int score = 0;
   if(current==dictionaryVerbs)
   {
-    run_verb_game(test_words);
+    score = run_verb_game(test_words);
   }
   else
   {
